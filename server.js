90c5d6bf988075cbc568057eb84a48c7c8e814b1
moonrifts <laaaybas@gmail.com>
2026-03-09 16:43:19 -0500

    Changed port.

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.get('/', (req, res) => {
     res.json({ message: 'Server is running!' })
 })
 
-app.listen(5000, () => {
-    console.log('Server running on port 5000')
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
